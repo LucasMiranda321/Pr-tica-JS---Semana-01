8fd7bc42356e4f441c8735d30bd3f009a18a317b
Lucas Miranda <>
2022-12-13 15:40:43 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+//Aula 04 - Sintaxe e tipos de Dados //
+
+const prompt = require('prompt-sync')();
+
+idade = 20;
+altura = 1.75;
+nome = "José";
+pagou = true;
+
+console.log("Os valores atribuídos às variáveis são: ");
+console.log("Idade: ", idade);
+console.log("Altura: ", altura);
+console.log("Nome: ", nome);
+console.log("Pagou: ", pagou);
+
+console.log("\nOs tipos das variáveis são: ");
+console.log("idade: ", typeof(idade));
+console.log("altura: ", typeof(altura));
+console.log("nome: ", typeof(nome));
+console.log("pagou: ", typeof(pagou));
